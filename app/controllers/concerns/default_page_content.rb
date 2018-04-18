d12d3ce4c12ada069dc46c76807c9e0cb0b5ea6c
Adam <a_prusse@hotmail.com>
2018-04-17 20:37:02 -0600

    updated file naming and tab name

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@
     end
 
     def set_page_defaults
-      @page_title = "Devcamp Portfolio | My Portfolio Website"
+      @page_title = "Adam Prusse Portfolio | My Portfolio Website"
       @seo_keywords = "Adam Prusse portfolio"
     end
   end
